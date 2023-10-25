6371a52e4a9361c23aaf97ad5082cf0af2582374
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-10-25 18:56:24 +0200

    feat: add table and pagination

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,6 +2,31 @@
 
 @section('content')
     <div class="container">
-        @dump($projects)
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Link</th>
+                    <th scope="col">Slug</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($projects as $project)
+                    <tr>
+                        <th scope="row">{{ $project->id }}</th>
+                        <td>{{ $project->title }}</td>
+                        <td>{{ $project->description }}</td>
+                        <td>{{ $project->link }}</td>
+                        <td>{{ $project->slug }}</td>
+                    </tr>
+                @empty
+                @endforelse
+
+            </tbody>
+        </table>
+
+        {{ $projects->links('pagination::bootstrap-5') }}
     </div>
 @endsection
